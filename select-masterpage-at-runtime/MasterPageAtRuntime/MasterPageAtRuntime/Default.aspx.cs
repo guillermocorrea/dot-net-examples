5e7b6c97fe8f6090d70e86771dfa539cc0040796
Guillermo Correa <guille_freelance@hotmail.com>
2013-11-06 10:34:42 -0500

    added change masterpage dinamically

---
+++ select-masterpage-at-runtime/MasterPageAtRuntime/MasterPageAtRuntime/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MasterPageAtRuntime
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public Default()
+        {
+            this.PreInit += new EventHandler(Default_PreInit);
+        }
+
+        void Default_PreInit(object sender, EventArgs e)
+        {
+            if (Request["ext"] != null && Request["ext"] == "1")
+            {
+                this.MasterPageFile = "~/MasterPages/Site2.Master";
+                return;
+            }
+
+            this.MasterPageFile = "~/MasterPages/Site1.Master";
+        }
+    }
+}
---
+++ select-masterpage-at-runtime/MasterPageAtRuntime/MasterPageAtRuntime/MasterPages/Site1.Master.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MasterPageAtRuntime.MasterPages
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        
+    }
+}
